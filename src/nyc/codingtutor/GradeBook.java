abaf494badfefeb2aed399416d3f0b8f3b24dec1
ZakAttakk <36577338+ZakAttakk@users.noreply.github.com>
2022-10-19 20:30:26 -0400

    updates

--- src/nyc/codingtutor/GradeBook.java
+++ src/nyc/codingtutor/GradeBook.java
@@ -1,9 +1,22 @@
 package nyc.codingtutor;
 
+import java.util.Scanner;
+
 public class GradeBook {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String fullName = "";
+		String lineVariable = "--------------------";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Grade Book Example");
+		System.out.println("Name of Course:");
+		String courseName = sc.nextLine();
+		System.out.println("How many students are in this class:");
+		int numOfStudents = sc.nextInt();
+		Student[] programmingStudents = new Student[numOfStudents];
+		
 
 	}
 
